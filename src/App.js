dc8a450338fc66c520cd7713f149a3f033f25f55
Ryurik <gearsrodryu@outlook.com>
2021-05-09 22:42:36 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to React!</h1>
+      </header>
+      <p className="App-intro">
+        Hi There!
+      </p>
+    </div>
+  );
+}
+
+export default App;
